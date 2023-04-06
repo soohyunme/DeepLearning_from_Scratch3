45cf54d7136a37e3041f1c8a569bd57ee4efd8da
soohyunme <soohyun.dev@gmail.com>
2023-04-06 16:55:20 +0900

    init

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -127,26 +127,6 @@ class Function:
         raise NotImplementedError()
 
 
-class Square(Function):
-    def forward(self, x):
-        return x**2
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
-
-
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-
-    def backward(self, gy):
-        x = self.input.data
-        gx = np.exp(x) * gy
-        return gx
-
-
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
@@ -210,20 +190,16 @@ class Pow(Function):
         return gx
 
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 
-def square(x):
-    return Square()(x)
-
-
-def exp(x):
-    return Exp()(x)
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
 
 
 def add(x0, x1):
@@ -264,40 +240,14 @@ def pow(x, c):
     return Pow(c)(x)
 
 
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
-
-
-def as_variable(obj):
-    if isinstance(obj, Variable):
-        return obj
-    return Variable(obj)
-
-
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__add__ = add
-Variable.__radd__ = add
-Variable.__neg__ = neg
-Variable.__sub__ = sub
-Variable.__rsub__ = sub
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
-Variable.__pow__ = pow
-
-x = Variable(np.array(2.0))
-y = -x
-print(y)
-
-y1 = 2.0 - x
-y2 = x - 1.0
-print(y1)
-print(y2)
-
-y = 3.0 / x
-print(y)
-
-y = x**3
-print(y)
+def setup_variable():
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__neg__ = neg
+    Variable.__sub__ = sub
+    Variable.__rsub__ = sub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__pow__ = pow
